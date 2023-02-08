7eebddb61125f9ff0eb893025bf5589aa7007081
Fal <92532021+FailedWare37@users.noreply.github.com>
2023-02-08 03:20:45 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+##MAIN SHIT / UGLY CODE
+import time
+i = 1 ##GAMELOOP
+ms = 0
+
+print("// PYTHON TIME WASTER SHITTER //")
+print(">EVERY 3 SECONDS CHANGES YOUR LEVEL BY 1*x")
+
+while i > 0:
+    print("[Time Wasted]:", "[",ms,"]")
+    ms +=1 
+    time.sleep(1)
